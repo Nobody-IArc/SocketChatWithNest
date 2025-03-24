ea7ab652bd74b577a8b31bda4bf39c9375f755d1
Nobody-IArc <utk3970@naver.com>
2025-03-24 19:12:32 +0900

    방 기능 생성

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -2,16 +2,61 @@ import {
   WebSocketGateway,
   WebSocketServer,
   SubscribeMessage,
+  MessageBody,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ namespace: 'chat' }) // 네임스페이스로 분리
 export class ChatGateway {
-  @WebSocketServer() server: Server;
+  @WebSocketServer()
+  server: Server;
 
+  // 메시지 전송
   @SubscribeMessage('message')
   handleMessage(socket: Socket, data: any): void {
     const { message, nickname } = data;
     socket.broadcast.emit('message', `${nickname} : ${message}`);
+  }
+}
+
+@WebSocketGateway({ namespace: 'room' }) // room namespace
+export class RoomGateway {
+  constructor(private chatGateway: ChatGateway) {}
+  rooms: any[] = [];
+
+  @WebSocketServer() // 서버 인스턴스 접근 변수
+  server: Server;
+
+  // 방 생성
+  @SubscribeMessage('createRoom') // createRoom 핸들러 메서드
+  handleMessage(@MessageBody() data: any) {
+    // ^ Socket 없이 데이터만
+    const { nickname, room } = data;
+    this.chatGateway.server.emit('notice', {
+      message: `${nickname} create ${room}`,
+    });
+    this.rooms.push(room); // room 정보 추가
+    this.server.emit('room-list', this.rooms); // room-list 전송
+  }
+
+  // 방 입장 시 알림
+  @SubscribeMessage('joinRoom')
+  async handleJoinRoom(socket: Socket, data: any): Promise<void> {
+    const { nickname, room, toLeaveRoom } = data;
+    await socket.leave(toLeaveRoom);
+    this.chatGateway.server.emit('notice', {
+      message: `${nickname} is joined ${room}`,
+    });
+    await socket.join(room);
+  }
+
+  // 방에서 메시지를 전송하는 기능
+  @SubscribeMessage('message')
+  handleMessageToRoom(socket: Socket, data: any) {
+    const { nickname, room, message } = data;
+    console.log(data);
+    socket.broadcast.emit('message', {
+      message: `${nickname} : ${message}`,
+    });
   }
 }
--- static/script.js
+++ static/script.js
@@ -1,16 +1,59 @@
 const socket = io('http://localhost:3000/chat');  // 네임스페이스 등록
 const nickname = prompt('Type your nickname');
+const roomSocket = io('http://localhost:3000/room');
+
+let currentRoom = ''; // 초기화
 
 socket.on('connect', () => {
   console.log('connected');
 });
 
+// 메시지 전송
 function sendMessage() {
+  if (currentRoom === '') {
+    return alert('Invalid room');
+  }
   const message = $('#message').val();
-  $('#chat').append(`<div>ME: ${message}</div>`); // 본인이 보낸 메시지 표시
-  socket.emit('message', { message, nickname });
+  const data = { message, nickname, room: currentRoom };
+
+  // html 파일에 추가할 것
+  $('#chat').append(`<div>ME : ${message}</div>`);
+  // emit 메서드 사용
+  roomSocket.emit('message', data);
+  return false;
 }
 
 socket.on('message', message => {
   $('#chat').append(`<div>${message}</div>`);
 });
+
+socket.on('notice', data => {
+  $('#notice').append(`<div>${data.message}</div>`);
+});
+
+
+// 방 생성
+function createRoom() {
+  const room = prompt('Type new Rooms name');
+  roomSocket.emit('createRoom', { room, nickname });
+}
+
+roomSocket.on('message', data => {
+  console.log(data);
+  $('#chat').append(`<div>${data.message}</div>`);
+});
+
+roomSocket.on('room-list', data => {
+  console.log(data);
+  $('#room-list').empty();
+  data.forEach((room) => {
+    $('#room-list').append(`<li>${room} <button onclick="joinRoom('${room}')">join</button></li>`);
+  });
+});
+
+// 방 입장
+function joinRoom(room) {
+  roomSocket.emit('joinRoom', { room, nickname, toLeaveRoom: currentRoom });
+  $('#chat').html('');  // 다른 방으로 이동 시 기존 대화 삭제
+  currentRoom = room;
+}
